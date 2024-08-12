d74e1dea0afeb35e67b30ed8c47b5e4f8b0c3bc2
Camilomachuca <Camilomachuca03@gmail.com>
2024-08-12 12:30:25 -0500

    feat: sparkles: a new function has been added

--- Python.py
+++ Python.py
@@ -123,7 +123,9 @@ while menuu==True:
         while buliano==True:# se utiliza un bucle while para crear los productos que el usuario requiera y asi añadirlos al json
             medicamento=input("escribe el nombre del producto: ")
             cantidad=int(input("cantidad: "))
-            precio=str(input("Precio: "))
+            for i in listaMedicamentos:
+                if medicamento== i["nombre"]:
+                    precio=i["precio"]
             medicamentosVendidos.append((medicamento, cantidad, precio))
             seguir=int(input("si deseas agregar otro producto escribe (si=1) o si deseas finalizar escribe (no=2)"))
             if seguir==1:
